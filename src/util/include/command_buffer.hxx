496b67211b27e394cbe27eb173825c15d3359c96
Seiji Emery <seijiemery@gmail.com>
2017-02-14 09:38:19 -0800

    Moved commandbuffer implementation to a new file w/ some fixes + improvements
    Note: still not tested (but *should* be copyable, movable, etc., and
    now has general-purpose macros and is now parameterized based on the
    command type to help avoid errors).
    
    This implementation is quite nice (if it works):
    – CommandBuffer parameterized based on a 'command key' enum class
    – 'command value(s)' are just structs with a void execute() method. No
    vtables / inheritance required, and the internal data structure can /
    will pack data
    
    Downsides:
    – Said data structure is extremely brittle (if misused)
    – Reading is currently a destructive operation (doesn't have to be, but
    is to keep the implementation simple).
    – Our use case is just: write stuff from thread X, copy to other data
    structures + read / dispatch from threads Y, Z, etc., so this should be
    fine though (I think?)
    
    TBD: CommandQueue implementation (threadsafe thing that manages
    CommandBuffers assigned to multiple threads), plus the threading
    infrastructure that manages that, etc.,

---
+++ src/util/include/command_buffer.hxx
@@ -0,0 +1,177 @@
+
+#pragma once
+
+template <size_t CHUNK_SIZE = 4096 * 4096>
+class ChunkedForwardList {
+    constexpr SIZE = CHUNK_SIZE - sizeof(size_t) * 2;
+    struct Chunk {
+        Chunk* next; size_t head = 0;
+        uint8_t data[SIZE];
+
+        Chunk () { std::fill(&data[0], &data[SIZE], 0); }
+        Chunk (const Chunk& chunk) {
+            memcpy(static_cast<void*>(this), static_cast<void*>(&chunk), sizeof(Chunk));
+            next = chunk->next ?
+                new Chunk(*chunk->next) :
+                nullptr;
+        }
+
+        template <typename T>
+        void write (const T& value, Chunk*& head) {
+            static_assert(sizeof(T) < SIZE);
+            if (SIZE - head <= sizeof(T)) {
+                if (!next) next = new Chunk();
+                head = next;
+                next->write(value);
+            }
+            *static_cast<T*>(&data[head]) = value;
+            head += sizeof(T);
+        }
+
+        template <typename T>
+        const T* read (Chunk*& head) {
+            static_assert(sizeof(T) < SIZE);
+            if (SIZE - head <= sizeof(T)) {
+                if (!next) return nullptr;
+                head = next;
+                return next->read(value);
+            }
+            auto ptr = static_cast<T*>(&data[head]);
+            head += sizeof(T);
+            return *ptr;
+        }
+        ~Chunk () { if (head) delete head; }
+    };
+    Chunk* head;
+    size_t unused;
+    Chunk first;
+
+    void resetHead () {
+        head = &first;
+        for (auto seg = head; seg; seg = seg->next)
+            seg->head = 0;
+    }
+    template <typename T>
+    void write (const T& value) {
+        head->write(value, head);
+    }
+    template <typename T>
+    const T* value read () {
+        return head->read(head);
+    }
+
+    ChunkedForwardList () : head(&first) {}
+    ChunkedForwardList (const ChunkedForwardList &cl) 
+        : head(nullptr), 
+          first(cl.first)    // copy chunk data recursively via Chunk ctor
+    {
+        // Iterate forward until we find the node that cl.head points to.
+        // Set the matching node in our list as our head.
+        for (auto src = &cl.first, dst = &first; src; src = src->next, dst = dst->next) {
+            assert((src != nullptr) == (dst != nullptr));
+            if (src == cl.head) 
+                head = dst;
+        }
+        assert(head != nullptr);
+    }
+};
+
+template <class CommandType>
+class CommandBuffer {
+    std::unique_ptr<ChunkedForwardList> buffer;
+    typedef CommandType CommandType;
+
+    CommandBuffer () : buffer(new decltype(buffer)());
+    CommandBuffer (CommandBuffer&& cb) : buffer(std::move(cb.buffer)) {}
+    CommandBuffer (const CommandBuffer& cb) : buffer(new decltype(buffer)(cb.buffer)) {}
+    virtual ~CommandBuffer () {}
+
+    void clear () { buffer->resetHead(); }
+
+    template <typename T>
+    void write (CommandType command, const T& data) {
+        buffer->write(command);
+        buffer->write(data);
+    }
+    CommandType readNext () {
+        auto ptr = buffer->read<CommandType>();
+        return ptr ? *ptr : CommandType::NONE;    // end of stream gets translated to command '0'.
+    }
+    template <typename T>
+    const T* read () {
+        return buffer->read<T>();
+    }
+};
+
+//
+// Macros to define user command buffers. See usage at end.
+//
+// If implemented, defines write(CommandBuffer<K>, const V& commandValue),
+// and dispatch(CommandBuffer<K>), which calls V::execute() for all matching V types.
+//
+
+// Defines a CommandBuffer dispatch method for a given CMD (enum class) type.
+//
+// CMD *must* define CMD::NONE = 0, which should not overlap with any other value.
+// Note: this is a special value and defining DEFINE_COMMAND(CMD::NONE, ...) 
+// or DISPATCH_COMMAND(CMD::NONE, ...) is illegal.
+//
+#define BEGIN_COMMAND_DISPATCH_IMPL(CMD) \
+void dispatch (CommandBuffer<CMD>& buffer) {\
+    bool done = false; \
+    E command; \
+    while (!done) { \
+        switch (CMD command = buffer.readNext()) { \
+            case CMD::NONE: atEnd = true; break;
+
+// End CommandBuffer dispatch method
+#define END_COMMAND_DISPATCH_IMPL(CMD) \
+            default: static_assert(0, "Missing command(s)"); \
+        } \
+    } \
+}
+
+// "Connect" an enum value (must be an element of CMD), and command structure type
+// (must have a void execute() method). Must occur between BEGIN / END _COMAND_DISPATCH_IMPL.
+#define DISPATCH_COMMAND(E,T) \
+    case EV: cb.read<T>->execute(); break;
+
+
+// Defines a CommandBuffer write method, "Connecting" an enum value and command structure type 
+// (must have a void execute() method). This is a freestanding declaration, bust must be matched
+// with a DISPATCH_COMMAND statement between BEGIN / END _COMMAND_DISPATCH_IMPL statements.
+#define DEFINE_COMMAND(E,T) \
+auto& write (CommandBuffer<decltype(E)>& buffer, const T& data) { \
+    return buffer.write(E, data), buffer; \
+}
+
+//
+// Macro usage / example:
+//
+
+#if 0
+
+// Command definition (header file)
+enum class kExampleCmd { NONE = 0, FOO, BAR, BAZ };
+namespace ExampleCommand {
+    struct Foo { ... void execute (); };
+    struct Bar { ... void execute (); };
+    struct Baz { ... void execute(); };
+}
+
+BEGIN_COMMAND_DISPATCH_IMPL(kExampleCmd)
+    DISPATCH_COMMAND(kExampleCmd::FOO, ExampleCommand::Foo)
+    DISPATCH_COMMAND(kExampleCmd::BAR, ExampleCommand::Bar)
+    DISPATCH_COMMAND(kExampleCmd::BAZ, ExampleCommand::Baz)
+END_COMMAND_DISPATCH_IMPL
+
+DEFINE_COMMAND(kExampleCmd::FOO, ExampleCommand::Foo)
+DEFINE_COMMAND(kExampleCmd::BAR, ExampleCommand::Bar)
+DEFINE_COMMAND(kExampleCmd::BAZ, ExampleCommand::Baz)
+
+// Command implementation (source file)
+void ExampleCommand::Foo::execute () { ... }
+void ExampleCommand::Bar::execute () { ... }
+void ExampleCommand::Baz::execute () { ... }
+
+#endif
